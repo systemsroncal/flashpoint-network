12bafb122f6c8e7489a1fe0f1b01b08afced2603
Enrique Roncal <systems.roncal@gmail.com>
2026-09-22 14:56:20 -0500

    Slightly reduce navy header bar height on desktop and mobile.

--- components/public/DesktopSiteHeader.tsx
+++ components/public/DesktopSiteHeader.tsx
@@ -246,7 +246,7 @@ export default function DesktopSiteHeader({
           <div className="absolute inset-x-0 top-0 z-30 h-[3px] bg-black" />
           <Link
             href="/"
-            className="absolute left-4 top-2.5 z-20 block md:left-8 lg:left-10"
+            className="absolute left-4 top-2 z-20 block md:left-8 lg:left-10"
             style={{
               width: logoWidths.desktop,
               maxWidth: logoWidths.desktop,
@@ -262,7 +262,7 @@ export default function DesktopSiteHeader({
             />
           </Link>
 
-          <div className="relative mx-auto max-w-[1920px] px-4 py-3 pl-[min(220px,20vw)] md:px-8 md:pl-[min(240px,22vw)] lg:px-10">
+          <div className="relative mx-auto max-w-[1920px] px-4 py-2 pl-[min(220px,20vw)] md:px-8 md:pl-[min(240px,22vw)] lg:px-10">
             <div className="grid grid-cols-[1fr_auto] items-center gap-4">
               <PrimaryNav
                 links={primaryLinks}
@@ -304,7 +304,7 @@ export default function DesktopSiteHeader({
     <header className="relative z-50 hidden w-full overflow-visible xl:block">
       <div className="relative overflow-visible border-b-[5px] border-[#E1B647] bg-[#000D3C] text-white">
         <div className="absolute inset-x-0 top-0 z-30 h-[3px] bg-black" />
-        <div className="relative mx-auto flex max-w-[1920px] items-center gap-6 overflow-visible px-4 pb-3 pt-[calc(3px+0.75rem)] md:gap-8 md:px-8 lg:px-10">
+        <div className="relative mx-auto flex max-w-[1920px] items-center gap-6 overflow-visible px-4 pb-2 pt-[calc(3px+0.5rem)] md:gap-8 md:px-8 lg:px-10">
           <LogoLink
             siteName={siteName}
             logoSrc={logoSrc}
